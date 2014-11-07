abfcf9cead8084b0b2ee5d90175e2b537c7d9117
ErichClark <erich.clark@gmail.com>
2014-11-07 09:29:17 -0500

    changes to Chapter 2 and MmmMethody creation

--- Chapter 2/Test/Test/Main.cs
+++ Chapter 2/Test/Test/Main.cs
@@ -28,24 +28,10 @@ class PortCalc
 		while(true)
 		{
 
-			Console.Write ("Enter port window radius between " + MIN_RADIUS + " and " + MAX_RADIUS + " inches or any negative number to exit:\n");
-			radiusString = Console.ReadLine();
-			radius = double.Parse(radiusString);
+			radius = readValue ("Enter port window radius between ", MIN_RADIUS, MAX_RADIUS);
 
-			if(radius < 0){
+			if (radius == 0){
 				break;
-			}
-
-			if  (radius > MAX_RADIUS) {
-				Console.WriteLine ("\n-ERROR- A window with a radius greater than " + MAX_RADIUS + " inches is unsafe! \n");
-				radius = MAX_RADIUS;
-				continue;
-			}
-
-			if  (radius < MIN_RADIUS) {
-				Console.WriteLine ("\n-ERROR- We can't make a port with a radius of less than " + MIN_RADIUS + " inches. \n");
-				radius = MIN_RADIUS;
-				continue;
 			}
 
 			holeRadius = radius + GASKET_THICKNESS;
@@ -60,7 +46,24 @@ class PortCalc
 			Console.WriteLine ( "Area in sq inches: {0,-20:0.00} Cost: ${1,-15:##,##0.00}", glassArea, glassCost ) ;
 			Console.WriteLine ( "Gasket in inches:  {0,-20:0.00} Cost: ${1,-15:##,##0.00}", gasketLength, gasketCost ) ;
 			Console.WriteLine ( "Total window cost: ${0,-20:0.00}", totalCost );
-			Console.WriteLine ( "INSTALLATION NOTE: This port will require a hole with a radius of " + holeRadius + " inches.\n");
+			Console.WriteLine ( "INSTALLATION NOTE: This port will require a hole with a radius of " + holeRadius + " inches.");
+			Console.WriteLine ( "Enter another port radius or 0 to exit program.\n");
 		}
+
+	}
+	static double readValue ( 
+	                         string prompt, // prompt for user
+	                         double min,    // lowest allowable value
+	                         double max     // largest allowable value
+	                         )
+	{
+		double result = 0;
+		do
+		{
+			Console.WriteLine (prompt + " between " + min + " and " + max );
+			string resultString = Console.ReadLine ();
+			result = double.Parse(resultString);
+		} while ( (result < min) || (result > max) || (result == 0 );
+		return result;
 	}
 }
--- Chapter 3/MmmMethody/Main.cs
+++ Chapter 3/MmmMethody/Main.cs
@@ -4,9 +4,37 @@ namespace MmmMethody
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		static int sillyReturnPlus (int i)
 		{
-			Console.WriteLine ("Hello World!");
+			i = i + 1;
+			Console.WriteLine ("i is: " + i);
+			return i;
+		}
+		public static void Main ()
+		{
+			int res;
+			res = sillyReturnPlus (5);
+			Console.WriteLine ("res is: " + res);
+
+			int min = 2;
+			int max = 10;
+			double pick = readValue ("Pick a number", min, max); 
+			Console.WriteLine ("You picked " + pick);
+		}
+		static double readValue ( 
+		                         string prompt, // prompt for user
+		                         double min,    // lowest allowable value
+		                         double max     // largest allowable value
+		                         )
+		{
+			double result = 0;
+			do
+			{
+				Console.WriteLine (prompt + " between " + min + " and " + max );
+				string resultString = Console.ReadLine ();
+				result = double.Parse(resultString);
+			} while ( (result < min) || (result > max) );
+			return result;
 		}
 	}
 }
